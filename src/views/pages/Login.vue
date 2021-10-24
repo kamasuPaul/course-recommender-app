669dd8395337229fe1ebf9b85b003cb17d958c1b
Kamasu Paul <kamasupaul@gmail.com>
2021-10-24 19:07:27 +0300

    add logging in

--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -4,40 +4,31 @@
       <v-card class="auth-card">
         <!-- logo -->
         <v-card-title class="d-flex align-center justify-center py-7">
-          <router-link
-            to="/"
-            class="d-flex align-center"
-          >
+          <router-link to="/" class="d-flex align-center">
             <v-img
               :src="require('@/assets/images/logos/logo.svg')"
               max-height="30px"
               max-width="30px"
               alt="logo"
               contain
-              class="me-3 "
+              class="me-3"
             ></v-img>
 
-            <h2 class="text-2xl font-weight-semibold">
-              Materio
-            </h2>
+            <h2 class="text-2xl font-weight-semibold">Course Recommender</h2>
           </router-link>
         </v-card-title>
 
         <!-- title -->
         <v-card-text>
-          <p class="text-2xl font-weight-semibold text--primary mb-2">
-            Welcome to Materio! 👋🏻
-          </p>
-          <p class="mb-2">
-            Please sign-in to your account and start the adventure
-          </p>
+          <p class="text-2xl font-weight-semibold text--primary mb-2">Welcome come back! 👋🏻</p>
+          <p class="mb-2">Please sign-in to your account and start the adventure</p>
         </v-card-text>
 
         <!-- login form -->
         <v-card-text>
           <v-form>
             <v-text-field
-              v-model="email"
+              v-model="form.body.email"
               outlined
               label="Email"
               placeholder="john@example.com"
@@ -46,7 +37,7 @@
             ></v-text-field>
 
             <v-text-field
-              v-model="password"
+              v-model="form.body.password"
               outlined
               :type="isPasswordVisible ? 'text' : 'password'"
               label="Password"
@@ -54,43 +45,30 @@
               :append-icon="isPasswordVisible ? icons.mdiEyeOffOutline : icons.mdiEyeOutline"
               hide-details
               @click:append="isPasswordVisible = !isPasswordVisible"
+              class="mb-3"
             ></v-text-field>
 
+            <template v-for="error in form.errors" >
+              <v-alert :key="error" :value="true" type="error" class="mb-3">
+                <small> {{ error[0] }}</small>
+              </v-alert>
+            </template>
+
             <div class="d-flex align-center justify-space-between flex-wrap">
-              <v-checkbox
-                label="Remember Me"
-                hide-details
-                class="me-3 mt-1"
-              >
-              </v-checkbox>
+              <v-checkbox v-model="form.remember" label="Remember Me" hide-details class="me-3 mt-1"> </v-checkbox>
 
               <!-- forgot link -->
-              <a
-                href="javascript:void(0)"
-                class="mt-1"
-              >
-                Forgot Password?
-              </a>
+              <a href="javascript:void(0)" class="mt-1"> Forgot Password? </a>
             </div>
 
-            <v-btn
-              block
-              color="primary"
-              class="mt-6"
-            >
-              Login
-            </v-btn>
+            <v-btn block color="primary" class="mt-6" :loading="loading" @click="login"> Login </v-btn>
           </v-form>
         </v-card-text>
 
         <!-- create new account  -->
         <v-card-text class="d-flex align-center justify-center flex-wrap mt-2">
-          <span class="me-2">
-            New on our platform?
-          </span>
-          <router-link :to="{name:'pages-register'}">
-            Create an account
-          </router-link>
+          <span class="me-2"> New on our platform? </span>
+          <router-link :to="{ name: 'auth-register' }"> Create an account </router-link>
         </v-card-text>
 
         <!-- divider -->
@@ -102,12 +80,7 @@
 
         <!-- social links -->
         <v-card-actions class="d-flex justify-center">
-          <v-btn
-            v-for="link in socialLink"
-            :key="link.icon"
-            icon
-            class="ms-1"
-          >
+          <v-btn v-for="link in socialLink" :key="link.icon" icon class="ms-1">
             <v-icon :color="$vuetify.theme.dark ? link.colorInDark : link.color">
               {{ link.icon }}
             </v-icon>
@@ -120,24 +93,14 @@
     <img
       class="auth-mask-bg"
       height="173"
-      :src="require(`@/assets/images/misc/mask-${$vuetify.theme.dark ? 'dark':'light'}.png`)"
-    >
+      :src="require(`@/assets/images/misc/mask-${$vuetify.theme.dark ? 'dark' : 'light'}.png`)"
+    />
 
     <!-- tree -->
-    <v-img
-      class="auth-tree"
-      width="247"
-      height="185"
-      src="@/assets/images/misc/tree.png"
-    ></v-img>
+    <v-img class="auth-tree" width="247" height="185" src="@/assets/images/misc/tree.png"></v-img>
 
     <!-- tree  -->
-    <v-img
-      class="auth-tree-3"
-      width="377"
-      height="289"
-      src="@/assets/images/misc/tree-3.png"
-    ></v-img>
+    <v-img class="auth-tree-3" width="377" height="289" src="@/assets/images/misc/tree-3.png"></v-img>
   </div>
 </template>
 
@@ -146,49 +109,84 @@
 import { mdiFacebook, mdiTwitter, mdiGithub, mdiGoogle, mdiEyeOutline, mdiEyeOffOutline } from '@mdi/js'
 import { ref } from '@vue/composition-api'
 
+const socialLink = [
+  {
+    icon: mdiFacebook,
+    color: '#4267b2',
+    colorInDark: '#4267b2',
+  },
+  {
+    icon: mdiTwitter,
+    color: '#1da1f2',
+    colorInDark: '#1da1f2',
+  },
+  {
+    icon: mdiGithub,
+    color: '#272727',
+    colorInDark: '#fff',
+  },
+  {
+    icon: mdiGoogle,
+    color: '#db4437',
+    colorInDark: '#db4437',
+  },
+]
 export default {
-  setup() {
-    const isPasswordVisible = ref(false)
-    const email = ref('')
-    const password = ref('')
-    const socialLink = [
-      {
-        icon: mdiFacebook,
-        color: '#4267b2',
-        colorInDark: '#4267b2',
-      },
-      {
-        icon: mdiTwitter,
-        color: '#1da1f2',
-        colorInDark: '#1da1f2',
-      },
-      {
-        icon: mdiGithub,
-        color: '#272727',
-        colorInDark: '#fff',
-      },
-      {
-        icon: mdiGoogle,
-        color: '#db4437',
-        colorInDark: '#db4437',
-      },
-    ]
-
+  data() {
     return {
-      isPasswordVisible,
-      email,
-      password,
+      isPasswordVisible: false,
+      loading: false,
       socialLink,
-
       icons: {
         mdiEyeOutline,
         mdiEyeOffOutline,
       },
+      rules: {
+        required: value => !!value || 'Required.',
+        min: v => v.length >= 6 || 'Min 6 characters',
+        emailMatch: () => "The email and password you entered don't match",
+      },
+      form: {
+        body: {
+          email: '',
+          password: '',
+        },
+        remember: false,
+
+        errors: {},
+        remember: false,
+        fetchUser: true,
+        autoLogin: true,
+        staySignedIn: false,
+      },
     }
+  },
+  methods: {
+    errors(res) {
+      this.form.errors = res.data.errors // Object.fromEntries(res.data.errors.map(item => [item.field, item.msg]))
+    },
+    login() {
+      this.loading = true
+      this.form.errors = null
+      this.$auth
+        .login({
+          url: '/login',
+          data: this.form.body,
+          redirect: { name: 'dashboard' },
+          remember: this.form.remember ? '{"name": "Redirect"}' : null,
+          fetchUser: false,
+          staySignedIn: true,
+        })
+        .then(null, res => {
+          this.errors(res.response)
+        })
+        .finally(() => {
+          this.loading = false
+        })
+    },
   },
 }
 </script>
-
 <style lang="scss">
 @import '~@/plugins/vuetify/default-preset/preset/pages/auth.scss';
 </style>
